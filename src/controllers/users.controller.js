6b9d9151ba0f71c0cd25c831bab899f4b0c6ec9e
FernandoLorca <florcapinto@gmail.com>
2023-07-15 15:48:32 -0400

    Cambie limitación de largo de contraseña de 15 a 20 caracteres

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,32 +1,32 @@
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
-import { usersModel } from "../models/users.model.js";
-import { handleErrors } from "../helpers/users.errors.js";
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import { usersModel } from '../models/users.model.js';
+import { handleErrors } from '../helpers/users.errors.js';
 
 //GENERA TOKEN SI SE VERIFICA LA EXISTENCIA DEL USUARIO Y LA CONTRASENA
 const tokenGen = async (req, res) => {
   const { email, password } = req.body;
   try {
     if (!email || !password) {
-      throw { code: "400" };
+      throw { code: '400' };
     }
     const result = await usersModel.readUser(email);
-    if (result === "404") {
-      throw { code: "404" };
-    }else if(result === "411") {
-      throw {code: "411"}
-    }else if (result.name === "error") {
+    if (result === '404') {
+      throw { code: '404' };
+    } else if (result === '411') {
+      throw { code: '411' };
+    } else if (result.name === 'error') {
       throw { code: result.code, message: result.message };
     }
     const verifyPass = await bcrypt.compare(password, result.password);
     if (!verifyPass) {
-      throw { code: "401" };
+      throw { code: '401' };
     }
     const token = jwt.sign(
       { email, password, role: result.role },
       process.env.JWT_PRIVATE_KEY,
       {
-        expiresIn: "1h",
+        expiresIn: '1h',
       }
     );
     res.json({ token });
@@ -41,17 +41,17 @@ const generateUser = async (req, res) => {
   const { email, password } = req.body;
   try {
     if (!email || !password) {
-      throw { code: "400" };
+      throw { code: '400' };
     }
-    if (password.length < 8 || password.length > 15) {
-      throw { code: "403" };
+    if (password.length < 8 || password.length > 20) {
+      throw { code: '403' };
     }
     const hashPass = await bcrypt.hash(password, 10);
     const result = await usersModel.createUser(email, hashPass);
-    if (result.name === "error") {
+    if (result.name === 'error') {
       throw { code: result.code, message: result.message };
     }
-    return res.json({ message: "Creado. Intente logear" });
+    return res.json({ message: 'Creado. Intente logear' });
   } catch (error) {
     const { status, message } = handleErrors(error.code, error.message);
     res.status(status).json({ status, message });
@@ -63,11 +63,11 @@ const getUser = async (req, res) => {
   const { email } = req.body;
   try {
     const result = await usersModel.readUser(email);
-    if (result === "404") {
-      throw { code: "404" };
-    }else if(result === "411") {
-      throw {code: "411"}
-    }else if (result.name === "error") {
+    if (result === '404') {
+      throw { code: '404' };
+    } else if (result === '411') {
+      throw { code: '411' };
+    } else if (result.name === 'error') {
       throw { code: result.code, message: result.message };
     }
     res.json(result);
@@ -80,28 +80,28 @@ const getUser = async (req, res) => {
 //EDITA USUARIO DE UN CAMPO A LA VEZ, PREVIA VALIDACION DE ROL
 const editUser = async (req, res) => {
   const { email, password, userToEdit, param, newParam } = req.body;
-  const token = req.headers.authorization.split(" ")[1];
+  const token = req.headers.authorization.split(' ')[1];
   const role = jwt.verify(token, process.env.JWT_PRIVATE_KEY).role;
   try {
     if (!userToEdit || !param || !newParam) {
-      throw { code: "405" };
+      throw { code: '405' };
     }
-    if (role === "user") {
+    if (role === 'user') {
       if (email === userToEdit) {
-        if (param === "id" || param === "role" || param === "created_at") {
-          throw { code: "406" };
+        if (param === 'id' || param === 'role' || param === 'created_at') {
+          throw { code: '406' };
         }
       } else {
-        throw { code: "407" };
+        throw { code: '407' };
       }
     }
-    if (param === "password") {
+    if (param === 'password') {
       newParam = await bcrypt.hash(password, 10);
     }
     const result = await usersModel.updateUser(userToEdit, param, newParam);
-    if (result === "404") {
-      throw { code: "404" };
-    } else if (result.name === "error") {
+    if (result === '404') {
+      throw { code: '404' };
+    } else if (result.name === 'error') {
       throw { code: result.code, message: result.message };
     }
     res.json(result);
