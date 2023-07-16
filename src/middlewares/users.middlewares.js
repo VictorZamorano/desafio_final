4dcf5eab866d594eb76e805ac8a39f0014760f80
NicoHndzHc <nico.hndz.s@gmail.com>
2023-07-16 19:13:43 -0400

    Se quita validacion de usuario en middleware y se pasa a tokenGen

--- src/middlewares/users.middlewares.js
+++ src/middlewares/users.middlewares.js
@@ -5,24 +5,14 @@ dotenv.config();
 
 export const verifyToken = (req, res, next) => {
   try {
-    const { email, password } = req.body;
-    if (!email || !password) {
-      throw { code: "400" };
-    }
     const bearerHeader = req.headers.authorization;
     if (!bearerHeader) {
       throw { code: "402" };
     }
     const token = bearerHeader.split(" ")[1];
     const payload = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
-    req.user_account_id = payload.user_id
-    // 
-    // console.log("payload", payload)
-    // console.log("req.user_account_id", req.user_account_id)
-    // 
-    if (email != payload.email || password != payload.password) {
-      throw { code: "410" };
-    }
+    req.email = payload.email;
+    req.password = payload.password;
     next();
   } catch (error) {
     const { status, message } = handleErrors(error.code, error.message);
